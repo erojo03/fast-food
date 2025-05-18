a3c28105d0e7f21460943898440290ec2ecc6d25
erojo03 <edgar.rojo03@outlook.com>
2025-05-17 19:33:23 -0500

    feat(app): integrate Navbar and routing with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './Router';
+import Navbar from './shared/components/navbar/Navbar';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <div className="flex h-dvh w-full flex-col bg-amber-50 md:flex-row-reverse">
+        <BrowserRouter>
+          <AppRouter />
+          <Navbar />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
