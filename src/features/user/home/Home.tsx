311f6921d7e61b39f719882a457318722548593a
erojo03 <edgar.rojo03@outlook.com>
2025-05-18 10:03:52 -0500

    feat(home): use Search and Categories components

--- src/features/user/home/Home.tsx
+++ src/features/user/home/Home.tsx
@@ -1,7 +1,11 @@
+import Search from "./components/Search.tsx";
+import Categories from "./components/Categories";
+
 const Home = () => {
   return (
     <main className="flex size-full flex-col gap-4 p-6">
-      <h1>Home</h1>
+      <Search />
+      <Categories />
     </main>
   );
 };
