eb1205a7cd21a56d000e6cbfce03d66694313814
erojo03 <edgar.rojo03@outlook.com>
2025-05-18 10:04:34 -0500

    feat(router): add redirect from "/" to "/app"

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './features/user/home/Home.tsx';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/app" element={<Home />} />
+
+      <Route path="/" element={<Navigate to="/app" />} />
     </Routes>
   );
 };
